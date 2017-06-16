5a6875a95383c01a70347644717ebbef7be85367
ANIRUDH-PC <ANIRUDH-PC@192.168.0.19>
2017-06-15 21:05:11 -0400

    Sample Project Commit

---
+++ SampleProject/src/testPackage/SampleClass.java
@@ -0,0 +1,17 @@
+package testPackage;
+
+public class SampleClass {
+
+	public static void add(int a , int b){
+		int c =0 ;
+		 c =a +b ;
+		 System.out.println("sum is :" + c );
+		 
+	}
+	
+	public static void main(String[] args) {
+		int a =10;
+		int b = 20;
+		add(a,b);
+	}
+}
